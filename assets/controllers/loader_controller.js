d1de8701b76d5c26c2857b84b28ff9fae6c64e79
Timothe Raynaud <raynaud.timothe@gmail.com>
2023-04-12 21:09:26 +0200

    :sparkles: Maj loadMore

--- assets/controllers/loader_controller.js
+++ assets/controllers/loader_controller.js
@@ -1,10 +1,11 @@
 import {Controller} from '@hotwired/stimulus'
 
 export default class extends Controller {
-    static targets = ['tricksContainer']
+    static targets = ['tricksContainer', 'buttonDisplayMore']
     static values = {
-        displayRecurrence : {type : Number, default : 2},
-        tricksByRow : Array,
+        displayRecurrence: {type: Number, default: 2},
+        tricksByRow: Array,
+        maxIteration: Number,
     }
 
     connect() {
@@ -12,66 +13,84 @@ export default class extends Controller {
             .then(response => response.json())
             .then(data => {
                 this.tricksByRowValue = data
-                this.displayTricks()
+                this.maxIterationValue = Math.min(this.displayRecurrenceValue, this.tricksByRowValue.length)
+                this.tricksContainerTarget.innerHTML = this.generateTricksHtml()
+                this.getDisplayMore()
             })
     }
 
-    displayTricks(){
-        this.tricksContainerTarget.innerHTML = this.generateTricksHtml()
+    // Generate the base of
+    generateTricksHtml() {
+        let tricksHtml = ''
+
+        for (let i = 0; i < this.maxIterationValue; i++) {
+            tricksHtml += this.getTricksCardHtml(i)
+        }
+
+        return tricksHtml
     }
 
-    loadMore(){
+    // Load one more row
+    loadMore() {
         this.displayRecurrenceValue = this.displayRecurrenceValue + 1
-        this.displayTricks()
-        if (this.displayRecurrenceValue === 3){
+        this.maxIterationValue = Math.min(this.displayRecurrenceValue, this.tricksByRowValue.length)
+        this.tricksContainerTarget.innerHTML += this.getTricksCardHtml(this.displayRecurrenceValue - 1)
+        this.getDisplayMore()
+        this.displayUpAnchor()
+    }
+
+    // Set html for displaying card tricks
+    getTricksCardHtml(i) {
+        let tricksHtml = ''
+
+        for (const trick of this.tricksByRowValue[i]) {
+            tricksHtml += `
+                <div class="card border-0 col-xl-2 col-lg-3 col-md-5 col-10 mx-2 p-0 tricks-card">
+                    <h2 class="card-header bg-primary text-uppercase">${trick.name}</h2>
+                    <div class="card-img-top">
+                        ${trick.image ?
+                `<img src="${this.assetPath(trick.image)}" alt="${trick.image.name}" height="220px" width="100%">` :
+                `<img src="${this.assetPath('images/tricks/default.jpg')}" alt="default" height="220px" width="100%">
+                        `}
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">${trick.description}</p>
+                    </div>
+                    <div class="card-footer bg-secondary">
+                        ${trick.type.name}
+                    </div>
+                </div>
+            `
+        }
+
+        return tricksHtml
+    }
+
+    // Display the button for load more
+    getDisplayMore() {
+        if (this.maxIterationValue !== this.tricksByRowValue.length) {
+            let tricksHtml = ''
+            tricksHtml += `
+                <a href="javascript:void(0)" data-action="click->loader#loadMore">
+                    <span>Afficher plus</span>
+                    <i class="fa-solid fa-chevron-down"></i>
+                </a>     
+            `
+            this.buttonDisplayMoreTarget.innerHTML = tricksHtml
+        } else {
+            this.buttonDisplayMoreTarget.innerHTML = '<div class="mb-5"></div>'
+        }
+    }
+
+    // Display anchor when 3 row is display
+    displayUpAnchor() {
+        if (this.displayRecurrenceValue === 3) {
             const anchor = document.getElementById('tricks-arrow-anchor-up')
             anchor.classList.remove('d-none')
         }
     }
 
-    generateTricksHtml() {
-        let tricksHtml = ''
-        const maxIterations = Math.min(this.displayRecurrenceValue, this.tricksByRowValue.length)
-
-        tricksHtml += '<div class="row g-4 px-4 justify-content-center">'
-        for (let i = 0; i < maxIterations; i++){
-
-            for (const trick of this.tricksByRowValue[i]) {
-                tricksHtml += `
-                    <div class="card border-0 col-xl-2 col-lg-3 col-md-5 col-10 mx-2 p-0 tricks-card">
-                        <h2 class="card-header bg-primary text-uppercase">${trick.name}</h2>
-                        <div class="card-img-top">
-                            ${trick.image ? 
-                                `<img src="${this.assetPath(trick.image)}" alt="${trick.image.name}" height="220px" width="100%">` : 
-                                `<img src="${this.assetPath('images/tricks/default.jpg')}" alt="default" height="220px" width="100%">
-                            `}
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text">${trick.description}</p>
-                        </div>
-                        <div class="card-footer bg-secondary">
-                            ${trick.type.name}
-                        </div>
-                    </div>
-                `
-            }
-
-            if (maxIterations !== this.tricksByRowValue.length && i === (maxIterations - 1) ){
-                tricksHtml += `
-                    <div id="load-more" class="col-10 mt-4" >
-                        <a href="javascript:void(0)" data-action="click->loader#loadMore">
-                            <span>Afficher plus</span>
-                            <i class="fa-solid fa-chevron-down"></i>
-                        </a>
-                    </div>
-                `
-            }
-        }
-        tricksHtml += '</div>'
-
-        return tricksHtml
-    }
-
+    // Path for image search
     assetPath(path) {
         const assetHost = this.assetHost()
         if (!assetHost) {
